70267aacea3eb6a6bed91cc811433a9e258aee00
artartur <arturgfs@hotmail.com>
2023-08-09 01:45:49 -0300

    Add Report Module in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ReportsModule } from './reports/reports.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ReportsModule);
   await app.listen(3000);
 }
 bootstrap();
